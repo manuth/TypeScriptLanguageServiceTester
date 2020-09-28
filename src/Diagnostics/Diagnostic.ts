65b3f0e9b0c8173c1abd46bf4bd5e64581ee7d0a
Manuel Thalmann <m@nuth.ch>
2020-09-28 21:56:52 +0200

    Fix the way to resolve the source of the diagnostics

--- src/Diagnostics/Diagnostic.ts
+++ src/Diagnostics/Diagnostic.ts
@@ -99,9 +99,10 @@ export class Diagnostic
      */
     public get Source(): string
     {
-        return Diagnostic.IsNormalDiagnostic(this.Diagnostic) ?
-            this.Diagnostic.source :
-            null;
+        return {
+            source: null,
+            ...this.Diagnostic
+        }.source;
     }
 
     /**
