4b394a015ce8ca6939ff980b649eb1c868ecd23b
Manuel Thalmann <m@nuth.ch>
2021-07-11 16:00:04 +0200

    Add missing export-statement

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { CodeAction } from "./Diagnostics/Actions/CodeAction";
 export { FixResponseAnalyzer } from "./Diagnostics/Actions/FixResponseAnalyzer";
 export { Diagnostic } from "./Diagnostics/Diagnostic";
 export { DiagnosticsResponseAnalyzer } from "./Diagnostics/DiagnosticsResponseAnalyzer";
+export { ICodeAnalysisResult } from "./Diagnostics/ICodeAnalysisResult";
 export { LanguageServiceTester } from "./LanguageServiceTester";
 export { TSServer } from "./TSServer";
 export { TestWorkspace } from "./Workspaces/TestWorkspace";
